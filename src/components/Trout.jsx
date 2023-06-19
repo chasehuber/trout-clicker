a7903a7453283bb294e0aea1b7be4dd13051c53e
Chase Huber <chasehuber23@gmail.com>
2023-06-19 12:46:35 -0400

    Added a function to make sure trout number doesn't get too large, it will shorten it down to words once you hit the millions

--- src/components/Trout.jsx
+++ src/components/Trout.jsx
@@ -7,6 +7,39 @@ import { PlayerContext } from "./PlayerContext";
 function Trout({ storeOpen }) {
   const {fishCount, setFishCount, troutPerSec} = useContext(PlayerContext)
 
+  // This calculates how many trout should be displayed, once you hit millions, billions, etc. it turns into 1.1 million
+  // and so on.
+  function displayedTrout() {
+    // According to someone on stack overflow, this would be faster than a switch statement
+    if(fishCount >= 1000000 && fishCount < 1000000000) {
+      let fixedAmount = (fishCount * 0.000001).toString().substring(0,5)
+      return(`${fixedAmount} Million`)
+    }
+
+    if(fishCount >= 1000000000 && fishCount < 1000000000000) {
+      let fixedAmount = (fishCount * 0.000000001).toString().substring(0,5)
+      return(`${fixedAmount} Billion`)
+    }
+
+    if(fishCount >= 1000000000000 && fishCount < 1000000000000000) {
+      let fixedAmount = (fishCount * 0.000000000001).toString().substring(0,5)
+      return(`${fixedAmount} Trillion`)
+    }
+
+    if(fishCount >= 1000000000000000 && fishCount < 1000000000000000000) {
+      let fixedAmount = (fishCount * 0.000000000000001).toString().substring(0,5)
+      return(`${fixedAmount} Quadrillion`)
+    }
+
+    if(fishCount > 1000000000000000000) {
+      return("Stop clicking... please....")
+    }
+
+    else {
+      return(Math.floor(fishCount))
+    }
+  }
+
   return (
     <div className={storeOpen ? "trout-box-hidden" : "trout-box"}>
       <div className="w-full">
@@ -15,7 +48,7 @@ function Trout({ storeOpen }) {
         </div>
 
         <div className="text-center text-5xl p-2 w-full bg-gray-800 bg-opacity-80">
-          <h1>{Math.floor(fishCount)} Trout</h1>
+          <h1>{displayedTrout()} Trout</h1>
           <h1 className="text-2xl">{(troutPerSec).toFixed(1)} TPS (Trout per second)</h1>
         </div>
       </div>
